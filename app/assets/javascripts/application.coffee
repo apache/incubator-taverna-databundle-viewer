168087d3b4681ee429ffd7d1e3e8d31d8ffd84ae
Denis Karyakin <samhane.me@gmail.com>
2015-06-08 15:32:45 +0300

    Updated makeup for login and registration pages

--- app/assets/javascripts/application.coffee
+++ app/assets/javascripts/application.coffee
@@ -30,3 +30,11 @@
 #
 #= require jquery2.min.js
 #= require bootstrap.min
+#= require icheck.min
+
+$ ->
+  $('input').iCheck
+    checkboxClass: 'icheckbox_square-blue'
+    radioClass: 'iradio_square-blue'
+    increaseArea: '20%'
+  return
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation) }
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
+  end
 end
