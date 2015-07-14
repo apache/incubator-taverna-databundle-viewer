83522584688e2a27cc8675f93f9eebe9beb746c9
Denis Karyakin <samhane.me@gmail.com>
2015-07-15 00:38:52 +0300

    Updated carrierwave config for use S3 only in production environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,18 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET'],
-      region: ENV['S3_REGION']
-  }
-  config.storage = :fog
-
   # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? # || Rails.env.development?
-    config.storage = :file
+  config.storage = :file
+
+  if Rails.env.test?
     config.enable_processing = false
     config.root = "#{Rails.root}/tmp"
+  elsif Rails.env.production?
+    config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: ENV['S3_KEY'],
+        aws_secret_access_key: ENV['S3_SECRET'],
+        region: ENV['S3_REGION']
+    }
+    config.storage = :fog
   end
 
   config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
