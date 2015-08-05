d60689cee5964eec81f84ee9b1c7cc2807fab3b3
Denis Karyakin <samhane.me@gmail.com>
2015-08-05 18:31:17 +0300

    Update config for carrierwave. Use S3 only if key present

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,21 @@
 CarrierWave.configure do |config|
   # For testing, upload files to local `tmp` folder.
   config.storage = :file
+  config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
 
   if Rails.env.test?
     config.enable_processing = false
     config.root = "#{Rails.root}/tmp"
   elsif Rails.env.production?
-    config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['S3_KEY'],
-        aws_secret_access_key: ENV['S3_SECRET'],
-        region: ENV['S3_REGION']
-    }
-    config.storage = :fog
+    if ENV['S3_KEY'].present?
+      config.fog_credentials = {
+          provider: 'AWS',
+          aws_access_key_id: ENV['S3_KEY'],
+          aws_secret_access_key: ENV['S3_SECRET'],
+          region: ENV['S3_REGION']
+      }
+      config.storage = :fog
+      config.fog_directory = ENV['S3_BUCKET_NAME']
+    end
   end
-
-  config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
-  config.fog_directory = ENV['S3_BUCKET_NAME']
 end
